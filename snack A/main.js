bdcf675e7e1fbad8df296d586f00191210eafc03
AlanDAmario <vega93@live.it>
2024-05-08 16:52:52 +0200

    [UPD] snack a with comments

--- snack A/main.js
+++ snack A/main.js
@@ -1,11 +1,17 @@
 'use strict';
+//CREAZIONE DI UN INPUT DI ARRAY 
 const arrElements = ['ciao', 'addio', 'benvenuto', 'salve'];
-contaElementi(arrElements);
-
+//CONTROLLARE SE LA FUNZIONE è CORRETTA, ULTIMO STEP
+console.log(contaElementi(arrElements));
+// CREAAZIONE FUNZIONE
 function contaElementi(arr) {
+    //CREARE UN CONTATORE CON VALORE 0, PERCHè L ARRAY PARTE DA 0
     let count = 0;
+    // CREAZIONE DEL LOOP WHILE (arr[count] STA A DEFINIRE CHE IL MIO PARAMETRO arr HA COME VALORE count)
     while (arr[count] !== undefined) {
+        //INCREMNETATORE 
         count++
     }
+    // COSA PORTARE A SCHERMO
     return count;
 }
